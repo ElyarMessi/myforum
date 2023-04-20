cd18dee497bd797ae76b5d2df4568fe09a695be3
elyar <1800016653@pku.edu.cn>
2023-04-20 16:06:50 +0800

    support markdown edit, develop my account page and some humanize details

--- boards/views.py
+++ boards/views.py
@@ -8,7 +8,7 @@ from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.core.urlresolvers import reverse
 
 
 def home(request):
@@ -43,7 +43,7 @@ class TopicListView(ListView):
     model = Topic
     context_object_name = 'topics'
     template_name = 'topics.html'
-    paginate_by = 20
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         kwargs['board'] = self.board
@@ -87,11 +87,16 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'topic_posts.html'
-    paginate_by = 2
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
     
@@ -111,7 +116,19 @@ def reply_topic(request, pk, topic_pk):
             post.topic = topic
             post.created_by = request.user
             post.save()
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+
+            topic.last_updated = timezone.now()
+            topic.save()
+
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url = topic_url,
+                id = post.pk,
+                page = topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
+            # return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
     
     else:
         form = PostForm()
